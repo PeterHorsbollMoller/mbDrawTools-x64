f78d4a2db9137504c8cf2d3ea85caf183c15b8fa
Peter Horsbøll Møller <peter@mller.dk>
2023-06-02 15:25:09 +0200

    3.0.1 and 3.0.2
    - Added support for editing M & Z values through the Node Editing tool
    - Added tool for updating all Z or M values on selected records
    - Removed call the OBJMZAddZSupport as Pro now can do this
    - Improved UpdateMZValue to also allow the user to select a column with values to use
    - Compiled with latest MBLIbraries

---
+++ Code/DrawTools.bat
@@ -0,0 +1,7 @@
+﻿@Echo on
+"C:\MapInfo\MapBasic\23.0\MapBasic.Exe" -NOSPLASH -D "DrawTools.mb" "DLGSetNodeCoordinate.mb"
+"C:\MapInfo\MapBasic\21.1\MapBasic.Exe" -NOSPLASH -L "DrawTools.mbp"
+
+Dir *.err
+
+Pause
