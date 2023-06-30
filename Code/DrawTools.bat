052159b5b1b96b8901f85c8fe8b50180c4044f5e
Peter Horsbøll Møller <peter@mller.dk>
2023-06-30 14:19:11 +0200

    DrawTools v3.0.6
    3.0.6	Added:
    - DTExtractNodesSelection: Extracts nodes into a new nodes table
    - DTUpdateZMUsingNodesTable: Updates nodes using points in a nodes table
    - Recompiled DrawTools and ObjectMZLib modules with MapBasic v2023 B47

--- Code/DrawTools.bat
+++ Code/DrawTools.bat
@@ -1,5 +1,5 @@
 ﻿@Echo on
-"C:\MapInfo\MapBasic\23.0\MapBasic.Exe" -NOSPLASH -D "DrawTools.mb" "DLGSetNodeCoordinate.mb"
+"C:\MapInfo\MapBasic\23.0\MapBasic.Exe" -NOSPLASH -D "DrawTools.mb" "DLGSetNodeCoordinate.mb" "DrawToolsMenu.mb" "DLGArrow.mb" "DLGIsoscelesTrapez.mb" "DLGSetNodeCoordinate.mb" "DLGUpdateZMValues.mb"
 "C:\MapInfo\MapBasic\21.1\MapBasic.Exe" -NOSPLASH -L "DrawTools.mbp"
 
 Dir *.err
